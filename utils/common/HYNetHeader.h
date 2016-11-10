4b476760712c87f0d10cdb06c8d8487b2c7adb49
龍情雪心 <qianjianlei@163.com>
2016-11-10 12:45:53 +0800

    1

---
+++ utils/common/HYNetHeader.h
@@ -0,0 +1,22 @@
+//
+//  HYNetHeader.h
+//  HappyYoga
+//
+//  Created by qianjianlei on 16/11/10.
+//  Copyright © 2016年 龍情雪心. All rights reserved.
+//
+
+#import "HYUtility.h"
+#import "HYGlobal.h"
+#import "HYGlobalParams.h"
+#import "HYNetWorkUtility.h"
+#import "LCAlertView.h"
+
+#ifndef YKTNetHeader_h
+#define YKTNetHeader_h
+
+static const NSInteger PAGESIZE30 = 30;
+static const NSInteger PAGESIZE10 = 10;
+
+
+#endif /* YKTGlobal_h */
